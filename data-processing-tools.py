33873cd9f51f7538fb834e183a5857d92ecb2cf3
Gabriel Alencar <gabriel_alencar2@hotmail.com>
2025-08-22 12:02:13 -0300

    create new tab with other providers processing

--- data-processing-tools.py
+++ data-processing-tools.py
@@ -10,6 +10,7 @@ from scripts.verifica_hashes_threads import verify_hashes
 from scripts.processa_meta_whats_logs import process_logs_extractions
 from scripts.create_hashes_model_file import create_hashes_file
 from scripts.hasher import Hasher
+from scripts.digital_guru_transformer import process_guru
 
 
 # COOKIE_FILE = "cookie.txt"
@@ -24,6 +25,10 @@ packages = {
     "natsort": "natsort",
     "pypdf": "pypdf",
 }
+
+PADYS = {"input_to_label": (3, 10), "label_to_top": (10, 0), "execute_button": 20}
+
+PROVIDER_TYPES_DICT = {"Digital Manager Guru": "Digital Manager Guru"}
 
 
 def check_install_packages(packages):
@@ -103,114 +108,150 @@ class Janela(ttk.Window):
             **kwargs,
         )
         self.title("Ferramentas de Processamento")
-        self.centralizar_janela(740, 460)
-        self.configura_abas()
-        self.configura_aba1()
-        self.configura_aba2()
-        self.configura_aba3()
-        self.configura_aba4()
+        self.centralize_window(900, 460)
+        self.configure_tabs()
+        self.configure_tab1()
+        self.configure_tab2()
+        self.configure_tab3()
+        self.configure_tab4()
+        self.configure_tab5()
 
-    def configura_abas(self):
-        self.abas = ttk.Notebook(self)
-        self.aba1 = ttk.Frame(self.abas)
-        self.aba2 = ttk.Frame(self.abas)
-        self.aba3 = ttk.Frame(self.abas)
-        self.aba4 = ttk.Frame(self.abas)
-        self.abas.add(self.aba1, text="1. Verificação de Hashes")
-        self.abas.add(self.aba2, text="2. Processamento de Logs")
-        self.abas.add(self.aba3, text="3. Gerar modelo hashes.txt (Listar Arquivos)")
-        self.abas.add(self.aba4, text="4. Comparar hash de um arquivo")
-        self.abas.pack(expand=1, fill="both")
+    def configure_tabs(self):
+        self.tabs = ttk.Notebook(self)
+        self.tab1 = ttk.Frame(self.tabs)
+        self.tab2 = ttk.Frame(self.tabs)
+        self.tab3 = ttk.Frame(self.tabs)
+        self.tab4 = ttk.Frame(self.tabs)
+        self.tab5 = ttk.Frame(self.tabs)
+        self.tabs.add(self.tab1, text="1. Verificar múltiplas hashes")
+        self.tabs.add(self.tab2, text="2. Processar logs e bilhetagem")
+        self.tabs.add(self.tab3, text="3. Gerar modelo hashes.txt (Listar Arquivos)")
+        self.tabs.add(self.tab4, text="4. Verificar hash de um arquivo")
+        self.tabs.add(self.tab5, text="5. Outros provedores")
+        self.tabs.pack(expand=1, fill="both")
 
-    def configura_aba1(self):
+    def configure_tab1(self):
         # === Aba 1: Verificação de Hashes ===
         ttk.Label(
-            self.aba1,
+            self.tab1,
             text="Pasta com arquivos (.zip, .gpg ou pdf) e arquivo de hashes (.txt, .csv ou .pdf):",
         ).pack(pady=(10, 0))
-        self.entry_pasta_hashes = ttk.Entry(self.aba1, width=path_input_width)
+        self.entry_pasta_hashes = ttk.Entry(self.tab1, width=path_input_width)
         self.entry_pasta_hashes.pack(pady=(3, 10))
         ttk.Button(
-            self.aba1,
+            self.tab1,
             text="Selecionar Pasta",
             command=lambda: selecionar_pasta(self.entry_pasta_hashes),
         ).pack()
 
         ttk.Button(
-            self.aba1, text="Verificar Hashes", command=self.executar_script_hashes
+            self.tab1, text="Verificar Hashes", command=self.executar_script_hashes
         ).pack(pady=40)
 
-    def configura_aba2(self):
+    def configure_tab2(self):
         # === Aba 2: Processamento de Logs ===
-        ttk.Label(self.aba2, text="Pasta com arquivos zips (WhatsApp, Meta):").pack(
+        ttk.Label(self.tab2, text="Pasta com arquivos zips (WhatsApp, Meta):").pack(
             pady=(10, 0)
         )
-        self.entry_pasta_logs = ttk.Entry(self.aba2, width=path_input_width)
+        self.entry_pasta_logs = ttk.Entry(self.tab2, width=path_input_width)
         self.entry_pasta_logs.pack(pady=(3, 10))
         ttk.Button(
-            self.aba2,
+            self.tab2,
             text="Selecionar Pasta",
             command=lambda: selecionar_pasta(self.entry_pasta_logs),
         ).pack()
-        # ttk.Label(self.aba2, text="Cookie de autorização:").pack(pady=(10, 0))
-        # self.entry_cookie = ttk.Entry(self.aba2, width=path_input_width)
+        # ttk.Label(self.tab2, text="Cookie de autorização:").pack(pady=(10, 0))
+        # self.entry_cookie = ttk.Entry(self.tab2, width=path_input_width)
         # self.entry_cookie.insert(0, carregar_cookie())
         # self.entry_cookie.pack(pady=(3, 0))
         ttk.Button(
-            self.aba2,
+            self.tab2,
             text="Processar Logs e Bilhetagem",
             command=self.executar_script_logs,
         ).pack(pady=20)
 
-    def configura_aba3(self):
+    def configure_tab3(self):
         # === Aba 3: Listagem de Arquivos ===
-        ttk.Label(self.aba3, text="Pasta com arquivos ZIP ou PDF:").pack(pady=(10, 0))
-        self.entry_pasta_listagem = ttk.Entry(self.aba3, width=path_input_width)
+        ttk.Label(self.tab3, text="Pasta com arquivos ZIP ou PDF:").pack(pady=(10, 0))
+        self.entry_pasta_listagem = ttk.Entry(self.tab3, width=path_input_width)
         self.entry_pasta_listagem.pack(pady=(3, 10))
         ttk.Button(
-            self.aba3,
+            self.tab3,
             text="Selecionar Pasta",
             command=lambda: selecionar_pasta(self.entry_pasta_listagem),
         ).pack()
         ttk.Button(
-            self.aba3,
+            self.tab3,
             text="Gerar modelo hashes.txt",
             command=self.executar_script_criacao_modelo,
         ).pack(pady=20)
 
-    def configura_aba4(self):
-        ttk.Label(self.aba4, text="Caminho do arquivo:").pack(pady=(10, 0))
-        self.entry_pasta_gerar_hash = ttk.Entry(self.aba4, width=path_input_width)
+    def configure_tab4(self):
+        ttk.Label(self.tab4, text="Caminho do arquivo:").pack(pady=(10, 0))
+        self.entry_pasta_gerar_hash = ttk.Entry(self.tab4, width=path_input_width)
         self.entry_pasta_gerar_hash.pack(pady=(3, 10))
         ttk.Button(
-            self.aba4,
+            self.tab4,
             text="Selecionar arquivo",
             command=lambda: selecionar_arquivo(self.entry_pasta_gerar_hash),
         ).pack()
 
         self.hash_type = ttk.StringVar()
-        ttk.Label(self.aba4, text="Tipo da hash:").pack(pady=(10, 0))
-        self.hash_box = ttk.Combobox(self.aba4, textvariable=self.hash_type)
+        ttk.Label(self.tab4, text="Tipo da hash:").pack(pady=(10, 0))
+        self.hash_box = ttk.Combobox(self.tab4, textvariable=self.hash_type)
         self.hash_box["values"] = ("SHA256", "SHA512")
         self.hash_box.state(["readonly"])
         self.hash_box.set("SHA256")
         self.hash_box.pack(pady=(3, 10))
 
-        ttk.Label(self.aba4, text="Hash original:").pack(pady=(10, 0))
-        self.entry_original_hash = ttk.Entry(self.aba4, width=path_input_width)
+        ttk.Label(self.tab4, text="Hash original:").pack(pady=(10, 0))
+        self.entry_original_hash = ttk.Entry(self.tab4, width=path_input_width)
         self.entry_original_hash.pack(pady=(3, 10))
 
         ttk.Button(
-            self.aba4, text="Comparar hash", command=self.comparate_single_file_hash
+            self.tab4, text="Comparar hash", command=self.comparate_single_file_hash
         ).pack(pady=20)
 
-    def centralizar_janela(self, largura, altura):
+    def configure_tab5(self):
+        self.provider_type = ttk.StringVar()
+        ttk.Label(
+            self.tab5, text="Selecione a empresa cujos dados serão processados:"
+        ).pack(pady=PADYS["label_to_top"])
+
+        values = tuple(PROVIDER_TYPES_DICT)
+        self.provider_box = ttk.Combobox(
+            self.tab5,
+            textvariable=self.provider_type,
+            state=("readonly"),
+            values=values,
+        )
+        self.provider_box.set(values[0])
+        self.provider_box.pack(pady=PADYS["input_to_label"])
+
+        # if self.provider_type == "":
+        ttk.Label(self.tab5, text="Caminho do arquivo .json:").pack(
+            pady=PADYS["label_to_top"]
+        )
+        self.entry_file_tab5 = ttk.Entry(self.tab5, width=path_input_width)
+        self.entry_file_tab5.pack(pady=PADYS["input_to_label"])
+
+        ttk.Button(
+            self.tab5,
+            text="Selecionar arquivo",
+            command=lambda: selecionar_arquivo(self.entry_file_tab5),
+        ).pack()
+
+        ttk.Button(self.tab5, text="Processar", command=self.execute_tab5).pack(
+            pady=PADYS["execute_button"]
+        )
+
+    def centralize_window(self, width, height):
         self.update_idletasks()
-        largura_tela = self.winfo_screenwidth()
-        altura_tela = self.winfo_screenheight()
-        x = (largura_tela // 2) - (largura // 2)
-        y = (altura_tela // 2) - (altura // 2) - 50
-        self.geometry(f"{largura}x{altura}+{x}+{y}")
+        screen_width = self.winfo_screenwidth()
+        screen_height = self.winfo_screenheight()
+        x = (screen_width // 2) - (width // 2)
+        y = (screen_height // 2) - (height // 2) - 50
+        self.geometry(f"{width}x{height}+{x}+{y}")
 
     def executar_script_hashes(self):
         pasta = self.entry_pasta_hashes.get()
@@ -299,6 +340,31 @@ class Janela(ttk.Window):
         except Exception as e:
             messagebox.showerror("Erro", f"Erro ao executar script:\n{e}")
 
+    def execute_tab5(self):
+        file = self.entry_file_tab5.get()
+        path = Path(file)
+
+        provider_type = self.provider_box.get()
+
+        match provider_type:
+            case value if value == PROVIDER_TYPES_DICT["Digital Manager Guru"]:
+                is_file_valid = self.validate_file(file)
+
+                if not is_file_valid:
+                    return
+
+                is_json = self.validate_is_json(path.resolve())
+
+                if not is_json:
+                    return
+
+                # process_guru(path.resolve())
+
+                messagebox.showinfo(
+                    "Sucesso",
+                    "Os arquivo processados foram salvo na pasta 'processamentos guru'",
+                )
+
     def validate_file(self, file):
         file_path = Path(file)
 
@@ -308,6 +374,15 @@ class Janela(ttk.Window):
 
         if file_path.is_dir():
             messagebox.showerror("Erro", "Selecione um arquivo, não um diretório")
+            return False
+
+        return True
+
+    def validate_is_json(self, file: str):
+        file_path = Path(file)
+
+        if file_path.suffix != ".json":
+            messagebox.showerror("Erro", "O arquivo selecionado não é um .json")
             return False
 
         return True
